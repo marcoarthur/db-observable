7a8d2cd1c720bbbd2af06d68aa859e00d8e7e2f6
Marco Silva <arthurpbs@gmail.com>
2025-07-07 17:38:55 -0300

    Initial

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+DB::Observable - It's new $module
+
+# SYNOPSIS
+
+    use DB::Observable;
+
+# DESCRIPTION
+
+DB::Observable is ...
+
+# LICENSE
+
+Copyright (C) Marco Silva.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Marco Silva <arthurpbs@gmail.com>
---
+++ lib/DB/Observable.pm
@@ -0,0 +1,39 @@
+package DB::Observable;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+DB::Observable - It's new $module
+
+=head1 SYNOPSIS
+
+    use DB::Observable;
+
+=head1 DESCRIPTION
+
+DB::Observable is ...
+
+=head1 LICENSE
+
+Copyright (C) Marco Silva.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Marco Silva E<lt>arthurpbs@gmail.comE<gt>
+
+=cut
+
